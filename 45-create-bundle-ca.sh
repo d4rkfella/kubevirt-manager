a2bd877e7bd90a9e95dd5b499a9b7735d1d4fbae
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-02-21 21:45:31 +0200

    Create 45-create-bundle-ca.sh

---
+++ 45-create-bundle-ca.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OUTPUT_FILE="/etc/ssl/certs/combined-ca-certificates.crt"
+
+CERTS_DIR="/etc/ssl/certs"
+
+> "$OUTPUT_FILE"
+
+add_cert_to_bundle() {
+    local cert_file="$1"
+    if [[ -f "$cert_file" ]]; then
+        if ! grep -qF "$(openssl x509 -in "$cert_file" -outform PEM)" "$OUTPUT_FILE"; then
+            echo "Adding $cert_file to the bundle..."
+            cat "$cert_file" >> "$OUTPUT_FILE"
+        else
+            echo "Skipping $cert_file (already in the bundle)"
+        fi
+    else
+        echo "Warning: $cert_file does not exist"
+    fi
+}
+
+for cert_file in "$CERTS_DIR"/*.{crt,pem}; do
+    add_cert_to_bundle "$cert_file"
+done
+
+echo "Certificates have been combined into $OUTPUT_FILE"
