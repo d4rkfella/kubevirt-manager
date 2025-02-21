d0c6d19310eb07098464d48e215fc970c3b0cfa4
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-02-21 02:58:49 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,6 @@
 
 set -e
 
-touch /var/log/nginx/access.log /var/log/nginx/error.log
-ln -sf /dev/stdout /var/log/nginx/access.log
-ln -sf /dev/stderr /var/log/nginx/error.log
-
 entrypoint_log() {
     if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
         echo "$@"
